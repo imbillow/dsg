203522789e5ad20d9e7e4d2d55cefc56fe4a6ddf
billow <billow.fun@gmail.com>
2025-02-08 17:48:12 +0800

    expr parser

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,28 @@ mod test {
     use crate::grammar;
 
     #[test]
-    fn test() {
+    fn term() {
         let t = grammar::TermParser::new();
         assert!(t.parse("1").unwrap() == 1);
         assert!(t.parse("(2)").unwrap() == 2);
         assert!(t.parse("((3))").unwrap() == 3);
+    }
+
+    #[test]
+    fn factor() {
+        let t = grammar::FactorParser::new();
+        assert!(t.parse("1*2").unwrap() == 2);
+        assert!(t.parse("(2)*3").unwrap() == 6);
+        assert!(t.parse("((8))/2").unwrap() == 4);
+    }
+
+    #[test]
+    fn expr() {
+        let t = grammar::ExprParser::new();
+        assert!(t.parse("1+2").unwrap() == 3);
+        assert!(t.parse("(2)*3+1").unwrap() == 7);
+        assert!(t.parse("2+((8))/2").unwrap() == 6);
+        assert!(t.parse("2+((8+2))/2").unwrap() == 7);
+        assert!(t.parse("2+((8*3))/2").unwrap() == 14);
     }
 }
